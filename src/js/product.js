dc202ca7f7a1304db8aba35f55dd45495eea6c7e
Costasamd <scosta1@byupathway.edu>
2025-11-05 16:01:55 +0000

    broken cart fixed

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,15 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+
+  const cartproduct = getLocalStorage("so-cart") || [];
+
+  cartproduct.push(product);
+
+  setLocalStorage("so-cart", cartproduct);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
