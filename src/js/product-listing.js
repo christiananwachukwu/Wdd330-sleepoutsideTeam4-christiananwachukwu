5af82d810016b5fe2ce7d7385aebcb5bcdabbf87
Christiana Nwachukwu <cnwachukwu4@byupathway.edu>
2025-11-17 02:05:34 +0100

    Fixed run lint errors

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,18 +1,18 @@
-import { loadHeaderFooter, getParam } from './utils.mjs';
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
 
 // Load header and footer
 loadHeaderFooter();
 
 //Get the category from the URL
-const category = getParam('category');
+const category = getParam("category");
 
 // Create an instance of ProductDate to fetch data
 const dataSource = new ProductData();
 
 // Get element where products will be displayed
-const listElement = document.querySelector('.product-list');
+const listElement = document.querySelector(".product-list");
 
 // Create an instance of ProductList with the category, data source, and element
 const myList = new ProductList(category, dataSource, listElement);
