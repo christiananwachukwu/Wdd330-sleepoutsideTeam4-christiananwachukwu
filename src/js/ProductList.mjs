e91191807139ae8774e20a0ad0861dfd277e2386
Christiana Nwachukwu <cnwachukwu4@byupathway.edu>
2025-11-18 20:45:28 +0100

    individual task discount flag

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,30 +1,37 @@
 import { renderListWithTemplate } from "./utils.mjs";
+ 
+function productCardTemplate(product) { 
+    const discount = product.ListPrice - product.FinalPrice;
+    let discountBadge = "";
 
-function productCardTemplate(product) {
+    if (discount > 0) {
+        const discountPercent = Math.round((discount / product.ListPrice) * 100);
+        discountBadge = `<span class="discount-badge">${discountPercent}% OFF</span>`;
+    }
+
     return `<li class="product-card">
-        <a href="/..product_pages/?product=${product.Id}">
+        <a href="/product_pages/?product=${product.Id}">
+          <div class="product-image-wrapper">
             <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name} ">
-            <h2 class="card__brand">${product.Brand.Name}</h2>
-            <h3 class="card__name">${product.NameWithoutBrand}</h3>
-            <p class="product-card__price">$${product.FinalPrice}</p>
+            ${discountBadge} <!-- Add discount flag here -->
+          </div>
+          <h2 class="card__brand">${product.Brand.Name}</h2>
+          <h3 class="card__name">${product.NameWithoutBrand}</h3>
+          <p class="product-card__price">$${product.FinalPrice}</p>
         </a>
-
     </li>`
 }
 
 export default class ProductList {
-    constructor(category, dataSource, listElements) {
+    constructor(category, dataSource, listElement) {
         this.category = category;
         this.dataSource = dataSource;
-        this.listElement = listElements;
+        this.listElement = listElement;
     }
 
     async init() {
         const list = await this.dataSource.getData(this.category);
         this.renderList(list);
-        document.querySelector(".title").textContent = this.category;
-
-
     }
 
     renderList(list) {
